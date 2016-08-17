8c85d8ba58e6841a4e236a3e37d9487caa1ff948
Madeleine <email": null>
2016-08-17 17:54:08 +1200

    Up to lesson 12, plus a little of lesson 13

--- lessons/11-productionish-server/modules/Repos.js
+++ lessons/11-productionish-server/modules/Repos.js
@@ -2,16 +2,38 @@ import React from 'react'
 import NavLink from './NavLink'
 
 export default React.createClass({
+
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    const path = `/repos/${userName}/${repo}`
+    this.context.router.push(path)
+    console.log("this is path: ", path)
+  },
+
   render() {
     return (
       <div>
-        <h2>Repos</h2>
-        <ul>
-          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
-          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
-        </ul>
-        {this.props.children}
-      </div>
+     <h2>Repos</h2>
+     <ul>
+       <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+       <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+       {/* add this form */}
+       <li>
+         <form onSubmit={this.handleSubmit}>
+           <input type="text" placeholder="userName"/> / {' '}
+           <input type="text" placeholder="repo"/>{' '}
+           <button type="submit">Go</button>
+         </form>
+       </li>
+     </ul>
+     {this.props.children}
+   </div>
     )
   }
 })

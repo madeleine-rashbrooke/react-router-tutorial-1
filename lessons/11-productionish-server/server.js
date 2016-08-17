0f8d79a16e3279155f8600d7ae77b4d19a2206dd
Madeleine <email": null>
2016-08-17 18:11:51 +1200

    All lessons finished. Actually works. Amazing.

--- lessons/11-productionish-server/server.js
+++ lessons/11-productionish-server/server.js
@@ -2,6 +2,11 @@ var express = require('express')
 var path = require('path')
 var compression = require('compression')
 
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { match, RouterContext } from 'react-router'
+import routes from './modules/routes'
+
 var app = express()
 
 app.use(compression())
@@ -10,9 +15,24 @@ app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')))
 
 // send all requests to index.html so browserHistory in React Router works
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.get('*', (req, res) => {
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    const appHtml = renderToString(<RouterContext {...props}/>)
+    res.send(renderPage(appHtml))
+    })
+  })
+
+function renderPage(appHtml) {
+  return `
+    <!doctype html public="storage">
+    <html>
+    <meta charset=utf-8>
+    <title>My First Router App</title>
+    <link rel=stylesheet href=/index.css>
+    <div id=app>${appHtml}</div>
+    `
+
+}
 
 var PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
